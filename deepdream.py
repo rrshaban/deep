bd9824b24e2721e6c45c26cca927f2eeb3a75a7f
Razi <rrshaban@users.noreply.github.com>
2015-09-11 11:40:18 -0400

    loading DNN model

--- deepdream.py
+++ deepdream.py
@@ -1,3 +1,10 @@
+# 
+# THIS WILL PROBABLY ONLY RUN ON 
+#   oil.cs.swarthmore.edu
+#
+#
+# trying out g's d-dream on oil - RS 9/11/15
+
 # imports and basic notebook setup
 from cStringIO import StringIO
 import numpy as np
@@ -12,8 +19,27 @@ import caffe
 caffe.set_mode_gpu()
 caffe.set_device(0)
 
-# def showarray(a, fmt='jpeg'):
-#     a = np.uint8(np.clip(a, 0, 255))
-#     f = StringIO()
-#     PIL.Image.fromarray(a).save(f, fmt)
-#     display(Image(data=f.getvalue()))
+def showarray(a, fmt='jpeg'):
+    a = np.uint8(np.clip(a, 0, 255))
+    f = StringIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    display(Image(data=f.getvalue()))
+
+model_path = '/local/caffe/models/bvlc_googlenet/' # oil.cs.swarthmore.edu:/local
+net_fn   = model_path + 'deploy.prototxt'
+param_fn = model_path + 'bvlc_googlenet.caffemodel'
+
+model = caffe.io.caffe_pb2.NetParameter()
+text_format.Merge(open(net_fn).read(), model)
+model.force_backward = True
+open('tmp.prototxt', 'w').write(str(model))
+
+
+net = caffe.Classifier('tmp.prototxt', param_fn,
+                       mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
+                       channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
+
+print net
+
+
+
