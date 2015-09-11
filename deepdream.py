a8a3d6bc7e7dbe9782b7d4bed1071a6ea5e98bfb
Razi <rrshaban@users.noreply.github.com>
2015-09-11 11:29:02 -0400

    test gpu setup

--- deepdream.py
+++ deepdream.py
@@ -7,3 +7,13 @@ from IPython.display import clear_output, Image, display
 from google.protobuf import text_format
 
 import caffe
+
+# test if cuda is set up on oil
+caffe.set_mode_gpu()
+caffe.set_device(0)
+
+# def showarray(a, fmt='jpeg'):
+#     a = np.uint8(np.clip(a, 0, 255))
+#     f = StringIO()
+#     PIL.Image.fromarray(a).save(f, fmt)
+#     display(Image(data=f.getvalue()))
