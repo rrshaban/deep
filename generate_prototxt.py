6129bd993346912c913d5d90602241d7d0c178da
Razi Shaban <rshaban1@swarthmore.edu>
2015-09-25 15:38:59 -0400

    repo restructure - made ./deepdream/

--- generate_prototxt.py
+++ generate_prototxt.py
@@ -4,6 +4,7 @@
 #   writing prototxt every time; also probably a lot easier to change. 
 #
 
+import caffe
 from caffe import layers as L
 from caffe import params as P
 
@@ -25,13 +26,25 @@ def lenet(lmdb, batch_size):
     return n.to_proto()
 
 def autoencoder():
+
     n = caffe.NetSpec()
-    n.data, n.label=L.Data(batch_size=100, backend=P.Data.LMDB, source=lmdb,
+
+    n.data, n.label=L.Data(batch_size=100, backend=P.Data.LMDB, source='mnist/mnist_train_lmdb',
                             transform_param=dict(scale=1./255), ntop=2)
+
+    n.ip1 = L.InnerProduct(n.data, num_output=784, weight_filler=dict(type='xavier'))
+    n.relu1 = L.ReLU(n.ip1, in_place=True)
+
+    n.ip2 = L.InnerProduct(n.relu1, num_output=392, weight_filler=dict(type='xavier'))
+    n.relu2 = L.ReLU(n.ip2, in_place=True)
+
+    n.ip3 = L.InnerProduct(n.relu2, num_output=784, weight_filler=dict(type='xavier'))
+
+    n.loss = L.SoftmaxWithLoss(n.ip3, n.data)
 
     return n.to_proto()
 
-print str(autoencoder)
+print autoencoder()
 
 # with open('mnist/lenet_auto_train.prototxt', 'w') as f:
 #     f.write(str(lenet('mnist/mnist_train_lmdb', 64)))
